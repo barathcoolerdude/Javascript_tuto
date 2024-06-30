ecba323e1aa9c53be0970b7c5c790a828dcc2b82
barathcoolerdude <barathelango3@gmail.com>
2024-06-30 13:06:39 +0530

    username

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-let age=25;
+/*let age=25;
 let price=10
 let gpa=3.5
-console.log(age);
-console.log("my age is ${age} years");
-console.log("prce of aplle is ${price} ");
-console.log("your gps is ${gpa}");
+console.log(`my age is ${age} years`);
+console.log(`prce of aplle is ${price} `);*/
+//console.log(`your gps is ${gpa}`);
+let username;
+
+document.getElementById("mysubmit").onclick=function(){
+    username=document.getElementById("mytext").value;
+    document.getElementById("myh1").textContent=`hello ${username}`;
+}
