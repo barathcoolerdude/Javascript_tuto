2d55664c5852acd149226ea0fc2f5b0784efb4d6
barathcoolerdude <barathelango3@gmail.com>
2024-07-09 23:41:36 +0530

    ternary operator

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-const myCheckbox=document.getElementById("myCheckbox");
-const visaBtn=document.getElementById("visaBtn");
-const masterCardBtn=document.getElementById("masterCardBtn");
-const paypalBtn=document.getElementById("paypalBtn");
-const mySubmit=document.getElementById("mySubmit");
-const subResult=document.getElementById("subResult");
-const paymentResult=document.getElementById("paymentResult");
-
-mySubmit.onclick=function(){
-    if(myCheckbox.checked){
-        subResult.textContent="you are subcribed";
-    }
-    else{
-        subResult.textContent="you are not subcribed";
-    }
-    if(visaBtn.checked){
-        paymentResult.textContent="you are paying with visa";
-    }
-    else if(masterCardBtn.checked){
-        paymentResult.textContent="you are paying with mastercard";
-    }
-    else if(paypalBtn.checked){
-        paymentResult.textContent="you are paying with paypal";
-    }
-    else{
-        paymentResult.textContent="you must select one";
-
-    }
-}
+let isStudent=true;
+let message = isStudent ? "you are a student": "you are not not a student";
+console.log(message)
