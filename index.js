6b367470b5e0253130f179c059495fc1603fdfae
barathcoolerdude <barathelango3@gmail.com>
2024-07-11 22:11:35 +0530

    switch

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-let isStudent=true;
-let message = isStudent ? "you are a student": "you are not not a student";
-console.log(message)
+let day = 15;
+switch(day%7){
+    case 1:
+        console.log("its monday");
+        break;
+        case 2:
+            console.log("its tuesday");
+            break;
+        case 3:
+            console.log("its wednesday");
+            break;
+        case 4:
+            console.log("its thursday");
+            break;
+        case 5:
+            console.log("its friday");
+            break;
+        case 6:
+            console.log("its saturday");
+            break;
+        case 0 :
+            console.log("its sunday");
+            break;
+}
