63d448677935cb8a550fe83ebefa4dd32bc99f5f
barathcoolerdude <barathelango3@gmail.com>
2024-07-14 10:01:39 +0530

    number_guessing_game

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-let day = 15;
-switch(day%7){
-    case 1:
-        console.log("its monday");
-        break;
-        case 2:
-            console.log("its tuesday");
-            break;
-        case 3:
-            console.log("its wednesday");
-            break;
-        case 4:
-            console.log("its thursday");
-            break;
-        case 5:
-            console.log("its friday");
-            break;
-        case 6:
-            console.log("its saturday");
-            break;
-        case 0 :
-            console.log("its sunday");
-            break;
+const minNum =1;
+const maxNum = 100;
+const answer= Math.floor(Math.random()*(maxNum-minNum))+minNum
+
+let attempt=0;
+let guess;
+let running = true;
+
+while(running==true){
+    guess=window.prompt(`guess number between ${minNum} - ${maxNum}`);
+    guess=Number(guess);
+    console.log(typeof guess,guess);
+    if(isNaN(guess)){
+        window.alert("enter a valid number");
+    }
+    else if(guess>maxNum || guess<minNum){
+        window.alert("enter betwwen the limits");
+    }
+    else{
+        attempt++;
+        if(guess>answer){
+            window.alert("the number is too high");
+        }
+        else if(guess<answer){
+            window.alert("the number is too low");
+        }
+        else{
+            if(guess==answer){
+                window.alert(`the guess is right ${guess} you got it right in ${attempt+1} attempts`)
+                running=false;
+            }
+            else{
+                window.alert(`the guess is wrong try again`);
+                
+            }
+        }
+    }
 }
